1d022b2d3a80bcbd12476188717b1f4c75fe5619
syatyo <yoshiharuseijin@gmail.com>
2020-03-22 17:26:23 +0900

    Tests translation from Int to String.

--- Tests/TranslateTests/TranslateTests.swift
+++ Tests/TranslateTests/TranslateTests.swift
@@ -2,9 +2,26 @@ import XCTest
 @testable import Translate
 
 final class TranslateTests: XCTestCase {
-    func testExample() {}
+    
+    struct IntegerToStringTranslator: TopLevelTranslator {
+        
+        func translate(from input: Int) -> String {
+            return String(input)
+        }
+    }
+    
+    func testTranslateIntToString() {
+        _ = [1, 2, 3]
+            .publisher
+            .translate(translator: IntegerToStringTranslator()) // Translate int to string.
+            .collect() // collect all elements into a single array.
+            .map { $0.reduce("", { $0 + $1 })} // sum all values in the array.
+            .sink { (result) in
+                XCTAssertEqual(result, "123")
+        }
+    }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testTranslateIntToString),
     ]
 }
